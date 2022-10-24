c858e8a1998911af33da6ef501d7912688c120fd
Klaster28 <89096914521@mail.ru>
2022-10-24 20:37:30 +0300

    Коммит после 1 задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
 
+    // задача 1
+    public static void DefinitionLeapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
-}
+
+    // Задача 2
+
+
+    // Задача 3
+
+        public static void main (String[]args){
+
+        DefinitionLeapYear(2025);
+
+
+
+
+
+
+
+
+
+        }
+    }
